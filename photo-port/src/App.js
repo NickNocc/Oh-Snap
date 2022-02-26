fe1bfc7f9254f8d8466ffbdb6ee76832fe6dda9e
NickNocc <haretakis4@gmail.com>
2022-02-26 11:11:09 -0800

    a

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import About from './components/about'
 
 function App() {
   return (
     <div>
-      
+
     </div>
   );
 }
