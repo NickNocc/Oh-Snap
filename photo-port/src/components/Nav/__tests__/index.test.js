0e3b154c11bfe4ee0e4353387666568939e2ad26
NickNocc <haretakis4@gmail.com>
2022-02-28 13:46:33 -0800

    Update index.test.js

--- photo-port/src/components/Nav/__tests__/index.test.js
+++ photo-port/src/components/Nav/__tests__/index.test.js
@@ -3,7 +3,7 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
-afterEach(cleanup);
+// afterEach(cleanup);
 
 describe('Nav component', () => {
     it('renders', () => {
