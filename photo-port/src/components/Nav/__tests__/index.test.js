d450ca82d28d6c6b6178045b1eec11eb946466b9
NickNocc <haretakis4@gmail.com>
2022-03-02 12:17:38 -0800

    tests done

--- photo-port/src/components/Nav/__tests__/index.test.js
+++ photo-port/src/components/Nav/__tests__/index.test.js
@@ -3,6 +3,15 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
+import ContactForm from '../../Contact';
+
+const categories = [
+  { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -34,4 +43,14 @@ describe('links are visible', () => {
     expect(getByTestId('about')).toHaveTextContent('About me');
   });
 
+})
+
+it('renders', () => {
+  render(<Nav
+    categories={categories}
+    setCurrentCategory={mockSetCurrentCategory}
+    currentCategory={mockCurrentCategory}
+    contactSelected={mockContactSelected}
+    setContactSelected={mockSetContactSelected}
+  />);
 })
